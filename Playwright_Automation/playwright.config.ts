01a3e69dcb34afd46f91da83b517bd220f5066af
elbalfqyasma@gmail.com <email-de-hajar@example.com>
2026-05-05 17:54:29 +0100

    Demo for CryptoJs

--- Playwright_Automation/playwright.config.ts
+++ Playwright_Automation/playwright.config.ts
@@ -4,10 +4,18 @@ import { defineConfig, devices } from "@playwright/test";
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+import dotenv from "dotenv";
+import path from "path";
 
+if (!process.env.NODE_ENV) {
+  // No environment specified → load default .env
+  require("dotenv").config({ path: `${__dirname}//src//config//.env` });
+} else {
+  // Environment specified → load .env.qa or .env.uat
+  require("dotenv").config({
+    path: `${__dirname}//src//config//.env.${process.env.NODE_ENV}`,
+  });
+}
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
--- Playwright_Automation/src/tests/loginTest.spec.ts
+++ Playwright_Automation/src/tests/loginTest.spec.ts
@@ -1,12 +1,14 @@
 import { test } from "@playwright/test";
 import HomePage from "../pages/HomePage";
+import { decrypt } from "../utils/CryptojsUtil";
+import { encryptEnvFile } from "../utils/EncryptEnvFile";
 
 test("test", async ({ page }) => {
   const homePage = new HomePage(page);
   await homePage.navigateToHomePage();
   const loginPage = await homePage.goToLoginPage();
-  await loginPage.fillUsername("demo@techmart.com");
-  await loginPage.fillPassword("demo123");
+  await loginPage.fillUsername(decrypt(process.env.userid!));
+  await loginPage.fillPassword(decrypt(process.env.password!));
   await loginPage.clickLoginButton();
   await homePage.expectLogoutButtonToBeVisible();
 });
